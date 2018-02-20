a795990b4dd52edfc80015af47daefee32c660eb
seanshuai06 <32368895+seanshuai06@users.noreply.github.com>
2018-02-20 02:16:05 -0800

    added single command tests

---
+++ tests/single_command.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Test 1: ls"
+echo "Test 2: echo This is my comment"
+echo "Test 3: mkdir aFold"
+echo "Test 4: df -h"
+
+echo "\nChanging directory to 'rshell...'"
+cd ..
+bin/rshell << EOF
+echo ls; ls
+echo echo This is my comment; echo This is my comment
+echo mkdir aFold; mkdir aFold
+echo df -h; df -h
+echo exit; exit
+EOF
